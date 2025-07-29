9c0d0c8cf5eac39301ece6530fa46b69db6019e0
unknown <mohammed98ali.ma@gmail.com>
2025-07-29 11:41:20 +0300

    file structure

---
+++ models/Combo.js
@@ -0,0 +1,25 @@
+const {model, Schema} = require("mongoose")
+
+const comboSchema = new Schema({
+    starter:{
+        type:String,
+        required: true,
+        
+    },
+    description:{
+        type:String,
+        required: true,
+    },
+    resource:{
+        type:String,
+        required: true,
+    }, 
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+})
+
+const Combo = model("Combo",comboSchema)
+
+module.exports = Combo
