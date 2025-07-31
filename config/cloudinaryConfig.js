327092749449868385d2fdfe3619762c5c70c2dc
unknown <mohammed98ali.ma@gmail.com>
2025-07-31 10:54:20 +0300

    troubleshooting video upload

---
+++ config/cloudinaryConfig.js
@@ -0,0 +1,21 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+cloudinary.config({
+    cloud_name: process.env.CLOUDNAME,
+    api_key: process.env.APIKEY,
+    api_secret: process.env.APISECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const storage = new CloudinaryStorage({
+  // cloudinary: cloudinary,
+  cloudinary,
+  params: {
+    allowed_formats: ['mp4','jpeg','jpg','png'],
+    folder: 'game-combos', // The name of the folder in cloudinary
+    // resource_type: 'raw'
+  }
+});
+//                     storage: storage
+module.exports = multer({ storage });
+
--- routes/combo.routes.js
+++ routes/combo.routes.js
@@ -3,12 +3,14 @@ const router = require("express").Router()
 const bcrypt = require("bcrypt")
 const multer = require("multer")
 const upload = multer({ dest: 'uploads/'})
+const fs = require('fs')
 const cloudinary = require("cloudinary").v2
 cloudinary.config({
     cloud_name: process.env.CLOUDNAME,
     api_key: process.env.APIKEY,
     api_secret: process.env.APISECRET // Click 'View API Keys' above to copy your API secret
 });
+const cloudinaryUploader = require("../config/cloudinaryConfig")
 
 
 
@@ -17,22 +19,60 @@ router.get("/new", (req,res)=>{
     res.render("combo.ejs")
 })
 
-router.post("/new", upload.single('video') , async(req,res)=> {
-     console.log(req.body)
+// router.post("/new", upload.single('video') , async(req,res)=> {
+//      console.log(req.body)
+//     try {
+//         await Combo.create(req.body)
+//         // req.file    
+//         res.redirect("/combos")
+//     } catch(error){
+//         console.log(error)
+//     }
+
+// })
+// Mohammad and Omar Route Cloudinary
+router.post("/omar",cloudinaryUploader.single("video"),async(req,res)=>{
+    console.log(req.file)
+})
+
+
+// for video upload: 
+router.post("/new", upload.single('video'), async (req, res) => {
     try {
-        await Combo.create(req.body)
-        // req.file    
+        if (req.session.user) {
+        let post = req.body
+        if (req.file) {
+            const uploadResult = await cloudinary.uploader
+                .upload(
+                    `./uploads/${req.file.filename}`,
+                    {
+                        resource_type: "video", // make sure video is the correct string/ "word" to specify it is a video.
+                        public_id: req.file.originalname
+                    }
+                )
+                .catch((error) => {
+                    console.log(error);
+                });
+            await fs.rmSync(`./uploads/${req.file.filename}`, {
+                force: true,
+            });
+            post.video = uploadResult.secure_url
+        }
+        post.file = null
+        post.creator = req.session.user._id
+        await Post.create(post)
         res.redirect("/combos")
-    } catch(error){
+    }
+    } catch (error){
         console.log(error)
     }
 
 })
 
-// router.post('/profile', upload.single('avatar'), function (req, res, next) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
-// })
+
+
+
+
 
 
 
@@ -47,7 +87,15 @@ router.get("/", async (req,res) => {
     }
 })
 
-
+router.get("/:comboId", async (req,res)=>{
+    try{
+        const oneComboDetail = await Combo.findById(req.params.comboId)
+        res.render("oneCombo.ejs", {oneComboDetail})
+    }
+    catch(error){
+        console.log(error)
+    }
+})
 
 
 
@@ -68,7 +116,28 @@ router.delete("/:comboId" , async (req,res)=>{
 
 
 
+// update: 
+router.get("/:comboId/update", async (req,res)=> {
+    try{
+        const foundCombo = await Combo.findById(req.params.comboId)
+        res.render("combo-update.ejs", {foundCombo})
+    }
+    catch(error){
+        console.log(error)
+    }
+})
 
+router.put("/:comboId", async (req, res)=>{
+    try{
+    const updatedCombo = await Combo.findByIdAndUpdate(req.params.comboId, req.body)
+    console.log(req.body)
+    console.log(updatedCombo)
+    res.redirect("/combos")
+    }
+    catch (error){
+        console.log(error)
+    }
+})
 
 
 
